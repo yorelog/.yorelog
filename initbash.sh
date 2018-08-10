03d8d07bd021d24bd308f5cbe383683fb50bd2b5
yorelog <yorelog@gmail.com>
2018-08-10 16:39:30 +0800

    fix install

--- initbash.sh
+++ initbash.sh
@@ -8,6 +8,7 @@ echo '[ -f '$BASEDIR'/.bashrc ] && source '$BASEDIR'/.bashrc' >> $HOME/.bashrc
 
 # install fasd
 cd $BASEDIR
+chmod +x fasd
 mv fasd $BINDIR
 #wget --no-check-certificate https://raw.githubusercontent.com/clvv/fasd/master/fasd -O fasd 
 #chmod +x fasd
