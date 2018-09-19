b3db30d02f00c98d6dd8932cd8317d0da399fc61
yorelog <yorelog@gmail.com>
2018-09-19 18:40:41 +0800

    use .zprofile for local variable assertion

--- init-zsh.sh
+++ init-zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASEDIR=$(pwd)
+BASEDIR=$(dirname "$0")
 
 # install Oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -9,8 +9,8 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 rm $HOME/.zshrc
 ln -s $BASEDIR/.zshrc $HOME/.zshrc
 
-if [ ! -e $HOME/.user_path ];then
-    touch $HOME/.user_path
-    echo "you can add some path variables in ~/.user_path"
+if [ ! -e $HOME/.zprofile ];then
+    ln -s $BASEDIR/.zprofile $HOME/.zprofile
+    echo "you can add some path variables in ~/.zprofile"
 fi
 
--- userpath.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(pwd)
-
-
-if [ ! -e $HOME/.user_paths ];then
-    echo "no file"
-fi
-
