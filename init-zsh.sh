7b1540cd0ead56416c423d4bca789aff58a563c7
yorelog <yorelog@gmail.com>
2020-07-29 15:35:26 +0800

    update init script

--- init-zsh.sh
+++ init-zsh.sh
@@ -2,22 +2,10 @@
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT")
 
-# install Oh my zsh
-if [[ $OSTYPE == *darwin* ]]; then
-    brew install zplug lua # use home brew to install zplug
-    export ZPLUG_HOME=/usr/local/opt/zplug
+ln -s $BASEDIR/.zshrc $HOME/.zshrc
+ln -s $BASEDIR/.zplug.zsh $HOME/.zplug.zsh
+#
+if [ ! -e $HOME/.zprofile ];then
+    cp $BASEDIR/.zprofile $HOME/.zprofile
+    echo "you can add some path variables in ~/.zprofile"
 fi
-#
-## cp zsh config file
-#if [ -e $HOME/.zshrc ];then
-#    rm $HOME/.zshrc
-#    echo "remove the default .zshrc"
-#fi
-#
-#ln -s $BASEDIR/.zshrc $HOME/.zshrc
-#
-#if [ ! -e $HOME/.zprofile ];then
-#    cp $BASEDIR/.zprofile $HOME/.zprofile
-#    echo "you can add some path variables in ~/.zprofile"
-#fi
-#
